b963d7440cb2524018dcfb5aa8335321da354f5c
Hein Swan Htet <hshscientist@gmail.com>
2024-03-13 15:24:47 +0630

    Created and added database connection Fixed bug: changed from config function reference to function call when importing dotenv module

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require('dotenv').config
+require('dotenv').config()
 require('express-async-errors')
 
 const express = require('express')
 const app = express()
 
-
+const connectDB = require('./db/connection')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleWare = require('./middleware/error-handler')
@@ -24,6 +24,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, () => {
             console.log(`Server is listening on http://localhost:${port}/`)
         })
