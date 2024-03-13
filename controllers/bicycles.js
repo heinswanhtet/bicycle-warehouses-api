80fad9ede1c8006bc303051a4973e4960172f135
Hein Swan Htet <hshscientist@gmail.com>
2024-03-13 21:01:35 +0630

    Added limiting and pagination functionalities when calling API

--- controllers/bicycles.js
+++ controllers/bicycles.js
@@ -38,6 +38,12 @@ const getAllBicycles = async (req, res) => {
         result = result.select(newFieldsList)
     }
 
+    // limiting & pagination
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+    result = result.skip(skip).limit(limit)
+
     const bicycles = await result
 
     res.status(200).json({ size: bicycles.length, bicycles })
