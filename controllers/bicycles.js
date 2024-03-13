1f3dabb3f0fb72c4a17071c8e1582c5a99fd84ac
Hein Swan Htet <hshscientist@gmail.com>
2024-03-13 20:15:52 +0630

    Added a featrue to query the data by "company"

--- controllers/bicycles.js
+++ controllers/bicycles.js
@@ -1,11 +1,15 @@
 const Bicycle = require('../models/Bicycle')
 
 const getAllBicycles = async (req, res) => {
-    const { featured } = req.query
+    const { featured, company } = req.query
     const queryObject = {}
 
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
+    }
+
+    if (company) {
+        queryObject.company = company
     }
 
     const bicycles = await Bicycle.find(queryObject)
