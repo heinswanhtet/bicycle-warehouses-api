833069b0940840959774c3c3806c556bf8775e13
Hein Swan Htet <hshscientist@gmail.com>
2024-03-13 20:48:07 +0630

    Added a functionality to sort the data

--- controllers/bicycles.js
+++ controllers/bicycles.js
@@ -1,7 +1,7 @@
 const Bicycle = require('../models/Bicycle')
 
 const getAllBicycles = async (req, res) => {
-    const { featured, company, name, released_date } = req.query
+    const { featured, company, name, released_date, sort } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -20,7 +20,19 @@ const getAllBicycles = async (req, res) => {
         queryObject.released_date = { $gt: new Date(released_date) }
     }
 
-    const bicycles = await Bicycle.find(queryObject)
+    let result = Bicycle.find(queryObject)
+
+    // sorting
+    if (sort) {
+        const newSortList = sort.replace(/\s/g, '').split(',').join(' ')
+        result = result.sort(newSortList)
+    }
+    // default sorting
+    else {
+        result = result.sort('createdAt')
+    }
+
+    const bicycles = await result
 
     res.status(200).json({ size: bicycles.length, bicycles })
 }
