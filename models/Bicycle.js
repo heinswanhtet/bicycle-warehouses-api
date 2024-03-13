c35caedcd5d07cc34e020bac3979e298de38fbfe
Hein Swan Htet <hshscientist@gmail.com>
2024-03-13 15:46:04 +0630

    Created bicycle schema

---
+++ models/Bicycle.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const bicycleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'bicycle name must be provided']
+    },
+    price: {
+        type: Number,
+        required: [true, 'bicycle price must be provided']
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    rating: {
+        type: Number,
+        default: 2.5
+    },
+    released: {
+        type: Date,
+        required: [true, 'bicycle release date must be provided']
+    },
+    company: {
+        type: String,
+        enum: {
+            values: ['trek', 'cervelo', 'kona', 'scott', 'marin', 'bianchi'],
+            message: '{VALUE} is not supported'
+        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date().now()
+    }
+})
+
+module.exports = bicycleSchema
